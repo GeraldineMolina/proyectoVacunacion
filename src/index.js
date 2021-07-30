8685137d99e19677a086da7f6d2584d49a860e5b
Gerald <gerald0805xd@gmail.com>
2021-07-30 11:06:31 -0500

    resolved confics

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <firebase firebase={firebase}>
-      <Suspense fallback={'Conectando la app..'}>
-        <App />
-      </Suspense>
-  </firebase>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
-
 
