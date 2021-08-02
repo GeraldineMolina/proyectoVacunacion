cd790aa068bfafd285d8fccd7efb5e3a13df0757
Leidy_Castro <dayanacf3@gmail.com>
2021-08-02 16:12:36 -0500

    Diseño de inicio de sesión terminado

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Jeringa from '../utils/images/Jeringa.png'
+import Logo from '../utils/images/Logo_Forja.png'
 
 const Login = (props) => {
 
@@ -15,35 +17,51 @@ const Login = (props) => {
     } = props;
 
     return(
-        <section className="login">
-            <div className="loginContainer">
-                <label>Username</label>
-                <input type="text" autoFocus required value={email} onChange={e => setEmail(e.target.value)} />
-                <p className="errorMsg">{emailError}</p>
+        <div className="Login2">
+              <div className="Logo">
+                    <img src={Logo} alt="este es nuestro logo" />
+                </div>
+            <section className="login">
+                <div className="Jeringa">
+                    <img src={Jeringa} alt="este es un simbolo" />
+                </div>
+                <div className="loginContainer">
+                    <p className="Title">Inicio de sesión</p>
+                   
+                    <input type="text" placeholder="Usuario" autoFocus required value={email} onChange={e => setEmail(e.target.value)} />
+                    <p className="errorMsg">{emailError}</p>
 
-                <label>Password</label>
-                <input type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
-                <p className="errorMsg">{PasswordError}</p>
-                    <div className="btnContainer">
-                        {hasAccount ? (
-                            <>
-                                <button onClick={handleLogin}>Iniciar Sesión</button>
-                                <p>
-                                ¿No tienes una cuenta?
-                                    <span onClick={() => setHasAccount(!hasAccount)}>Registrate</span>
-                                </p>
-                            </>
+                    
+                    <input type="password" placeholder="Contraseña" required value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <p className="errorMsg">{PasswordError}</p>
+                        <div className="btnContainer">
+                            {hasAccount ? (
+                                <>
+                                <button onClick={handleSignup}>Registrarse</button>
+                                <p>Tienes una cuenta ? <span onClick={() => setHasAccount(!hasAccount)}>Inicia Sesión</span></p>
+                                    {/* <button onClick={handleLogin}>Iniciar Sesión</button>
+                                    <p>
+                                    ¿No tienes una cuenta?
+                                        <span onClick={() => setHasAccount(!hasAccount)}>Registrate</span>
+                                    </p> */}
+                                </>
 
-                        ) : (
-                            <>
-                            <button onClick={handleSignup}>Registrarse</button>
-                            <p>Tienes una cuenta ? <span onClick={() => setHasAccount(!hasAccount)}>Inicia Sesión</span></p>
-                            </>
-                        )}
-                    </div>
-            </div>
+                            ) : (
+                                <>
+                                <button type="button" onClick={handleLogin}>Continuar</button>
+                                    <p>
+                                    
+                                        <span onClick={() => setHasAccount(!hasAccount)}></span>
+                                    </p>
+                                {/* <button onClick={handleSignup}>Registrarse</button>
+                                <p>Tienes una cuenta ? <span onClick={() => setHasAccount(!hasAccount)}>Inicia Sesión</span></p> */}
+                                </>
+                            )}
+                        </div>
+                </div>
 
-        </section>
+            </section>
+        </div>
     )
 }
 
