10ae2d99ef669d2d754b0587c23542507a1074c3
Gerald <gerald0805xd@gmail.com>
2021-08-03 11:32:29 -0500

    i made changes in the crud, i created the base layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,13 +2,14 @@ import React from 'react';
 import '../utils/css/estilos_hero.css';
 import { TableContainer , Table , TableHead , TableBody , TableRow , TableCell} from '@material-ui/core';
 import forjaicono from '../utils/images/Logo_Forja.png';
+import editar from '../utils/images/iconodeeditar.png'
 const data= [
-    {id: 1000728673 , nombre: 'Geraldine Molina', sintomalogia: 'Dolores de cabeza  y dolor de garganta.', dosis: 'Primera', laboratorio: 'laboratorio Rh', editar: 'editar' },
-    {id: 1008090611 , nombre: 'Natalia Gomez', sintomalogia: 'Tos, dificulatd para respirar y fiebre de 38º', dosis: 'Segunda', laboratorio: 'Cafam', editar: 'editar' },
-    {id: 10738695 , nombre: 'Camilo Arias', sintomalogia: 'Dolores de cabeza y tos.', dosis: 'Primera', laboratorio: 'laboratorio clinico', editar: 'editar' },
-    {id: 101013157 , nombre: 'Andrea Castro', sintomalogia: 'Tos, dificulatd para respirar y fiebre de 38º', dosis: 'Primera', laboratorio: 'laboratorio Rh', editar: 'editar' },
-    {id: 1077895574 , nombre: 'Yamile rincon', sintomalogia: 'Dolores de cabeza con dificultad para respirar.', dosis: 'Ninguna', laboratorio: 'laboratorio clinico', editar: 'editar' },
-    {id: 1055687137 , nombre: 'Samuel Herrera', sintomalogia: 'Tos, dificulatd para respirar y fiebre de 38º', dosis: 'Primera', laboratorio: 'Cafam', editar: 'editar' },
+    {id: 1000728673 , nombre: 'Geraldine Molina', sintomalogia: 'Dolores de cabeza  y dolor de garganta.', dosis: 'Primera', laboratorio: 'laboratorio Rh' },
+    {id: 1008090611 , nombre: 'Natalia Gomez', sintomalogia: 'Tos, dificulatd para respirar y fiebre de 38º', dosis: 'Segunda', laboratorio: 'Cafam' },
+    {id: 10738695 , nombre: 'Camilo Arias', sintomalogia: 'Dolores de cabeza y tos.', dosis: 'Primera', laboratorio: 'laboratorio clinico' },
+    {id: 101013157 , nombre: 'Andrea Castro', sintomalogia: 'Tos, dificulatd para respirar y fiebre de 38º', dosis: 'Primera', laboratorio: 'laboratorio Rh' },
+    {id: 1077895574 , nombre: 'Yamile rincon', sintomalogia: 'Dolores de cabeza con dificultad para respirar.', dosis: 'Ninguna', laboratorio: 'laboratorio clinico' },
+    {id: 1055687137 , nombre: 'Samuel Herrera', sintomalogia: 'Tos, dificulatd para respirar y fiebre de 38º', dosis: 'Primera', laboratorio: 'Cafam' },
 
 ];
 
@@ -44,12 +45,12 @@ const Hero = (props) => {
                                 <Table className="tabla" > 
                                     <TableHead className="tablacabecera"> 
                                         <TableRow className="tablaRow"> 
-                                            <TableCell> Id </TableCell>
-                                            <TableCell> Nombre </TableCell>
-                                            <TableCell> Sintomalogia </TableCell>
-                                            <TableCell> Dosis </TableCell>
-                                            <TableCell> Laboratorio </TableCell>
-                                            <TableCell> Editar </TableCell>
+                                            <TableCell><h2 className="row"> Id </h2></TableCell>
+                                            <TableCell><h2 className="row1">Nombre</h2></TableCell>
+                                            <TableCell><h2 className="row1"> Sintomalogia</h2></TableCell>
+                                            <TableCell><h2 className="row1">Dosis </h2></TableCell>
+                                            <TableCell><h2 className="row1">Laboratorio</h2></TableCell>
+                                            <TableCell><h2 className="row1">Editar</h2></TableCell>
                                         </TableRow>
                                     </TableHead>
 
@@ -61,7 +62,7 @@ const Hero = (props) => {
                                                 <TableCell> {celda.sintomalogia} </TableCell>
                                                 <TableCell> {celda.dosis} </TableCell>
                                                 <TableCell> {celda.laboratorio} </TableCell>
-                                                <TableCell> {celda.editar} </TableCell>
+                                                <TableCell> <img  className="editar" src={editar} /></TableCell>
 
                                             </TableRow>
                                         
